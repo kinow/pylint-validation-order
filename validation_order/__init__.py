192d402381842529263a0708bda77c2ff4a1dcf9
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-01-06 19:29:59 +1300

    Type hinting

--- validation_order/__init__.py
+++ validation_order/__init__.py
@@ -1,7 +1,11 @@
 from . import validation_order
+from pylint.lint import PyLinter
 
 __all__ = ['validation_order', 'register']
 
 
 def register(linter):
+    """
+    :type linter: PyLinter
+    """
     linter.register_checker(validation_order.ValidationOrderChecker(linter))
