5f0bd72fafd9f2a86f62f86f08d357513e32b7b0
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-01-14 15:37:52 +1300

    Add more tests for good case

--- validation_order/tests/test_validation_order.py
+++ validation_order/tests/test_validation_order.py
@@ -7,7 +7,30 @@ from validation_order.validation_order import ValidationOrderChecker
 class TestValidationOrder(CheckerTestCase):
     CHECKER_CLASS = ValidationOrderChecker
 
-    def test_bad_validation_order(self):
+    def test_good_validation_order_0(self):
+        stmt = astroid.extract_node(
+            '''
+        def some_method():
+            n = get_n_value()
+            if n > 2:
+                raise Exception("Invalid value for N!")
+        '''
+        )
+        with self.assertNoMessages():
+            self.walk(stmt)
+
+    def test_good_validation_order_1(self):
+        stmt = astroid.extract_node(
+            '''
+        def some_method():
+            if self.today_date > 2:
+                raise Exception("Invalid value for N!")
+        '''
+        )
+        with self.assertNoMessages():
+            self.walk(stmt)
+
+    def test_bad_validation_order_dangling_variable(self):
         stmt = astroid.extract_node(
             '''
         def some_method(n):
